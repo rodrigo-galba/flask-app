e013970e33018cbc52f17f0f5c4f8180d16f375a
Rodrigo Galba Miranda <rodrigogalba@gmail.com>
2020-03-27 20:18:50 -0300

    add simple health check and fake parameter list

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask           # import flask
+from flask import Flask, jsonify           # import flask
+from service import ParamService
+
+import json
+
 app = Flask(__name__)             # create an app instance
 
 @app.route("/")                   # at the end point /
 def hello():                      # call method hello
     return "Hello World!"         # which returns "hello world"`
 
-@app.route("/<name>")              # at the end point /<name>
-def hello_name(name):              # call method hello_name
-    return "Hello "+ name          # which returns "hello + name 
+@app.route("/health")
+def health():
+    return "200"
+
+@app.route("/list", methods=["GET"])
+def list_params():
+    return jsonify(ParamService().list())
 
 if __name__ == "__main__":        # on running python app.py
-    # app.run()                   # run the flask app 
-    app.run(debug=True)           # run the flask app using debugger
+    app.run()                   # run the flask app
+    # app.run(debug=True)           # run the flask app using debugger
